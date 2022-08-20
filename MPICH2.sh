e3962f2f50a23aa670ef351cb3ed6417d29808c3
Mark Wang <71631906+markwang0@users.noreply.github.com>
2022-08-20 01:04:36 -0700

    force appending of mpich-install/bin to $PATH

--- MPICH2.sh
+++ MPICH2.sh
@@ -6,9 +6,9 @@ tar xzf mpich-3.2.1.tar.gz
 mkdir mpich
 cd mpich-3.2.1
 export FFLAGS="-w -fallow-argument-mismatch -O2" # see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=91731
-./configure --prefix=$WORK2/TauDEMDependencies/mpich/mpich-install 2>&1 | tee c.txt
+./configure --prefix=$HOME/TauDEMDependencies/mpich/mpich-install 2>&1 | tee c.txt
 make 2>&1 | tee m.txt
 make install 2>&1 | tee mi.txt
 
-# Add the following to .bashrc
-PATH=$WORK2/TauDEMDependencies/mpich/mpich-install/bin:$PATH ; export PATH
+# Add to .bashrc
+echo "export PATH=$HOME/TauDEMDependencies/mpich/mpich-install/bin:$PATH" >> ~/.bashrc
