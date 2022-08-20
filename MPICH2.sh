ce8a61005fc555bf19894b19282a7b7f9d3825bc
Mark Wang <71631906+markwang0@users.noreply.github.com>
2022-08-20 00:57:51 -0700

    add flag to avoid fortran compiler error

--- MPICH2.sh
+++ MPICH2.sh
@@ -5,6 +5,7 @@ wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz --no-check-c
 tar xzf mpich-3.2.1.tar.gz
 mkdir mpich
 cd mpich-3.2.1
+export FFLAGS="-w -fallow-argument-mismatch -O2" # see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=91731
 ./configure --prefix=$WORK2/TauDEMDependencies/mpich/mpich-install 2>&1 | tee c.txt
 make 2>&1 | tee m.txt
 make install 2>&1 | tee mi.txt
